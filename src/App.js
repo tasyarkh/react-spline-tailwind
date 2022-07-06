65605f2a36d218afc83c32bb7793cce153467d48
tasyarkh <tasyaramadhinta@gmail.com>
2022-07-06 14:57:25 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import './index.css';
-import Nav from './components/Nav';
 import Name from './components/Name';
 import Me from './components/Me';
 import Experience from './components/Experience';
@@ -9,7 +8,6 @@ import Contact from './components/Contact';
 function App() {
   return (
     <div className='flex max-w-screen min-h-screen flex-col items-center justify-center relative bg-bck pb-20 overflow-hidden overscroll-contain'>
-    <Nav/>
     <Name/>
     <main className='w-[90%] mt-2'>
       <Me/>
--- src/components/Me.js
+++ src/components/Me.js
@@ -16,12 +16,13 @@ function Me() {
          <div className='w-full h-420 flex flex-col items-center justify-center px-3'>
 
            <p  className='text-lg text-txt text-left'>
-            hallo !!
-            introduce my name Tasya Ramadhinta K, you can call me Sya, I am 17 years old, I have an interest as a frontend developer and UI / UX designer
+            
+            Hallo !!
+            introduce my name Tasya Ramadhinta Khoirunnisa, you can call me Sya, I am 17 years old, I have an interest as a frontend developer and UI / UX designer
            </p>
 
            <motion.button whileTap={{scale: 0.8}}  className='w-full md:w-auto relative mt-6 inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-decTwo to-decThree group-hover:from-decOne group-hover:to-decTwo hover:text-white dark:text-white focus:ring-4 focus:ring-pink-400 dark:focus-within:ring-decOne hover:shadow-lg hover:shadow-decThree hover:dark:shadow-lg hover:dark:shadow-decThree ' >
-             <span className='w-full md:w-auto relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0'> Contact Me</span>
+             <span className='w-full md:w-auto relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0'><a href="https://api.whatsapp.com/send?phone=62895328483971">Contact Me</a></span>
            </motion.button>
          </div>
        </section>
